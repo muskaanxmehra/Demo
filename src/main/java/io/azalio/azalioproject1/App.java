93135fbf1ff221bedf7fea220675123de31c8518
muskaanxmehra <muskanxxmehra21@icloud.com>
2025-11-29 13:33:17 +0530

    Initial commit

---
+++ src/main/java/io/azalio/azalioproject1/App.java
@@ -0,0 +1,14 @@
+package io.azalio.azalioproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is my maven project");
+    }
+}
---
+++ src/main/java/io/azalio/azalioproject1/Test.java
@@ -0,0 +1,5 @@
+package io.azalio.azalioproject1;
+
+public class Test {
+
+}
