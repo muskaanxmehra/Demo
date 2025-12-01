2ef8b47cb01e7545f0416aadbb2a24b13ee7036b
muskaanxmehra <muskanxxmehra21@icloud.com>
2025-12-01 13:46:10 +0530

    Minor Change

--- src/main/java/io/azalio/azalioproject1/Test2.java
+++ src/main/java/io/azalio/azalioproject1/Test2.java
@@ -2,7 +2,7 @@ package io.azalio.azalioproject1;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("In Azalio , I am working as a Software Engineer");
+		System.out.println("Bye Bye");
 	}
 
 }
